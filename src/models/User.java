6e80b77808da79b4d5d47908d18de837594133ae
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-04-08 20:29:41 -0300

    calculate library delayed days

---
+++ src/models/User.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author devapp
+ */
+public class User {
+
+    private String name;
+    private String course;
+    private String period;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+    
+    /**
+     * @return the course
+     */
+    public String getCourse() {
+        return course;
+    }
+
+    /**
+     * @param course the course to set
+     */
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    /**
+     * @return the period
+     */
+    public String getPeriod() {
+        return period;
+    }
+
+    /**
+     * @param period the period to set
+     */
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+}
